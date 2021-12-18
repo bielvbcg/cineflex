e602eb85f6170c3d04b980469a3f55eae541fc07
bielvbcg <gabrielvbcallegario@gmail.com>
2021-12-18 16:22:56 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import EscolhaFilme from "./EscolhaFilme"
+import Header from "./Header"
+
+export default function App(){
+  return (
+    <>
+      <Header />
+      <EscolhaFilme/>
+    </>
+  )
+}
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styled from 'styled-components';
+
+export default function Header(){
+  return (
+    <Titulo>
+      <span>CINEFLEX</span>
+    </Titulo>
+  )
+}
+
+const Titulo = styled.div`
+  heigth: 67px;
+`
+
