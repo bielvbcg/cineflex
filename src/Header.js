85e6a4644deb47743ffdf95c78985b270d0c26a8
bielvbcg <gabrielvbcallegario@gmail.com>
2021-12-18 16:33:13 -0300

    header pronto

--- src/Header.js
+++ src/Header.js
@@ -10,6 +10,19 @@ export default function Header(){
 }
 
 const Titulo = styled.div`
-  heigth: 67px;
+  height : 67px;
+  background-color: #C3CFD9;
+
+  display : flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  span {
+    font-size: 34px;
+    line-height: 40px;
+
+    color: #E8833A;
+}
 `
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {render} from "react-dom";
 import App from "./App.js"
+import "./reset.css"
 
 render(<App/> , document.querySelector(".root"))
